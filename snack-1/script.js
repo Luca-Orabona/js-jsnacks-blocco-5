4a217ba1174d58e39b31b7f2134818c99bb9559c
Luca-Orabona <lucaorabona94@gmail.com>
2025-05-08 15:17:59 +0200

    feat: snack-1 snack-2 completed

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,39 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+
+// ciclo for
+let strName = "";
+for(let i = 0; i < names.length; i++) {
+
+    const name = names[i];
+    strName += `"${name}" `;
+}
+
+console.log(strName);
+
+
+
+// ciclo for...of
+let strName2 = "";
+
+for(let name of names) {
+
+    strName2 += `"${name}" `;
+}
+
+console.log(strName2);
+
+
+// metodo forEach
+let strName3 = "";
+names.forEach((name) => strName3 += `"${name}" `);
+console.log(strName3);
+
+
+
+
+
+
+
--- snack-2/script.js
+++ snack-2/script.js
@@ -6,3 +6,37 @@ const people = [
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+// ciclo for
+let peopleName = "";
+for(let i = 0; i < people.length; i++) {
+
+  const curPeople = people[i];
+  /* const {name} = curPeople; // possibilità di farlo anche con destrutturazione dell'oggetto.
+  peopleName += `"${name}" `; */
+  peopleName += `"${curPeople.name}" `;
+}
+
+console.log(peopleName);
+
+
+// ciclo for...of 
+let peopleName2 = "";
+for(let curPeople of people) {
+ /*  const {name} = curPeople; // possibilità di farlo anche con destrutturazione dell'oggetto.
+  peopleName2 += `"${name}" `; */
+  peopleName2 += `"${curPeople.name}" `;
+}
+
+console.log(peopleName2);
+
+
+//ciclo forEach
+let peopleName3 = "";
+
+people.forEach((curPeople) => peopleName3 += `"${curPeople.name}" `);
+console.log(peopleName3);
+
+
+
+
